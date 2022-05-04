6ce30b13c5c8b9ed4fd7d2c8d896adb8a930fabb
Maureen Cohen <s1144983@sms.ed.ac.uk>
2022-05-04 20:51:38 +0100

    Added aerosol mmr to pyburn

--- exoplasim/pyburn.py
+++ exoplasim/pyburn.py
@@ -152,7 +152,8 @@ ilibrary = {"110":["mld"  ,"mixed_layer_depth"               ,"m"          ],
             "406":["dftu"  ,"longwave_up"                    ,"W m-2"      ],
             "407":["dftd"  ,"longwave_down"                  ,"W m-2"      ],
             "408":["dtdt"  ,"rad_heating_rate"               ,"K s-1"      ],
-            "409":["dfdz"  ,"flux_convergence"               ,"W m-3"      ]}
+            "409":["dfdz"  ,"flux_convergence"               ,"W m-3"      ],
+			"410":["mmr"   ,"aerosol_mass_mixing_ratio"      ,"kg kg-1"    ]}
 
 #dictionary that can be searched by string codes
 slibrary = {}
